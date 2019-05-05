2233407f9ee20b0dd2f527a09446348c0bc7938f
wangliheng <wangliheng@k2data.com.cn>
2019-05-05 17:59:27 +0800

    标签管理页面添加

--- KfcTag.vue
+++ KfcTag.vue
@@ -1,10 +1,90 @@
 <template>
-  <div></div>
+  <KfcTable
+    style="margin-top: 20px"
+    :headers="table.headers"
+    :datas="table.dataList"
+    :total="table.total"
+    ref="table"
+    @on-page-change="onPageChange" />
 </template>
 
 <script>
+// import { Button } from 'iview'
+import KfcTable from './KfcTable'
 export default {
-  name: 'KfcTag'
+  name: 'KfcTag',
+  components: {
+    KfcTable
+  },
+  data () {
+    return {
+      table: {
+        headers: [
+          { title: '标签ID', key: 'id', width: '' },
+          { title: '标签名称', key: 'tagName', width: '' },
+          { title: '引用数量', key: 'num', width: '' },
+          { title: '操作',
+            width: '',
+            render: (h, params) => {
+              return h('div', [
+                h('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.editTag(params.index)
+                    }
+                  }
+                }, '编辑'),
+                h('Button', {
+                  props: {
+                    type: 'error',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.removeTag(params.index)
+                    }
+                  }
+                }, '删除')
+              ])
+            }
+          }
+        ],
+        dataList: [],
+        pageNo: 1,
+        pageSize: 10,
+        total: 0
+      }
+    }
+  },
+  mounted () {
+    this.reloadTable()
+  },
+  methods: {
+    reloadTable () {
+      this.$axios.get('/tag').then(res => {
+        this.table.dataList = res.data.body.tags
+        this.table.total = res.data.body.tags.length
+      })
+    },
+    onPageChange (pageNo, pageSize) {
+      this.table.pageNo = pageNo
+      this.table.pageSize = pageSize
+      this.reloadTable()
+    },
+    editTag () {
+
+    },
+    removeTag () {
+
+    }
+  }
 }
 </script>
 
